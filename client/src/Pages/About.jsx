723385162d3e5eff0e419cf5169a268bd9e473f2
Gsalive <gsalive2002@gmail.com>
2023-11-27 00:19:46 +0530

    Second commit

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -15,7 +15,7 @@ function About(){
     return(
         <div>
             <motion.div  
-            initial={{ opacity: 0,y:50 }}
+            initial={{ opacity: 0,y:100 }}
             animate={{ opacity: 1 ,y:0}}
             transition={{ 
                 duration: 0.9,delay:0.2}}>
